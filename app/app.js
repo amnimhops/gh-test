d1554e712b0b5edaa3d16bb363383114fef5caa9
fubar <fu@bar>
2020-01-26 21:50:29 +0100

    Añadido enlace app/web

--- app/app.js
+++ app/app.js
@@ -1,4 +1,9 @@
-class App{
-    constructor(){
-    }
-}
+import { ListService } from "./services/listService.js";
+import { ListController } from "./components/lists/listController.js";
+import { ListView } from "./components/lists/listView.js";
+import { ListModel } from "./components/lists/listModel.js";
+
+let service = new ListService(jQuery.ajax);
+let model = new ListModel();
+let view = new ListView(model, jQuery);
+let controller = new ListController(view, model, service, window.localStorage, jQuery);
